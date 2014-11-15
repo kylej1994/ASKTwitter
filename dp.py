6cbb0471232fd07ad6a718433a703d1d7777de18
Alec S <linuxbash8@gmail.com>
2014-11-15 13:54:21 -0600

    Fixed library

--- dp.py
+++ dp.py
@@ -20,8 +20,14 @@ for i in range(len(ws)):
 	else:
 		dp[ws[i]]=[]
 		dp[ws[i]].append(ws[i+1])
+numsyls=random.randrange(8,10)
+syl=0
 while(not (start=="END")):
 	print(start +" ",end="")
+	syl+=lib.getSylls(start)
+	if(syl>=numsyls):
+		print("")
+		syl=0
 	i=random.randrange(0,len(dp[start]))
 	start=dp[start][i]
 	if(start=="END"):
--- lib.py
+++ lib.py
@@ -1,43 +1,17 @@
+import nltk
+from nltk.corpus import cmudict
+d=cmudict.dict()
 def remPunc(x):
 	ret=""
 	for c in x:
-		if(c.isalpha()):
+		if(c.isalpha()or isdigit(c)):
 			ret=ret+c
 	return ret.lower()
 
-def isVowel(c):
-	if((c=='a')or(c=='e')or(c=='i')or(c=='o')or(c=='u')):
-		return True
-	else:
-		return False
-
-def isNotE(c):
-	return (isVowel(c)and(not(c=='e'))
+def isdigit(c):
+	digs=['1','2','3','4','5','6','7','8','9','0']
+	return c in digs
 
 def getSylls(word):
-	i=0
-	count=0
-	while(i<range(len(word))):
-		if(i==len(word-1)):
-			if(isNotE(word[i])):
-				count+=1
-				break
-			break
-		elif(isVowel(word[i])and(isVowel(word[i+1])):
-			word=word[:i]+word[i+1:]
-		elif(word[i]=='e'):
-			if(not(i==len(word)-1)):
-				count+=1
-		elif(isVowel(word[i])):
-			count+=1
-		elif(word[i]=='y'):
-			if(i==0):
-				if(len(word)==1):
-					count+=1
-					break
-				elif(not(isVowel(word[i+1]))):
-					count+=1
-			else:
-				if(i==len(word)-1):
-					if(not(isVowel(word[i-1]))):
-						count+=1
+	return [len(list(y for y in x if isdigit(y[-1]))) for x in d[word]][0]
+
