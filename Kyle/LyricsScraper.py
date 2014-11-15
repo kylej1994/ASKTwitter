08a349f3ab548d5bcf9c4bff3da2b9a1c87a5940
kylej1994 <kylejablon@Kyles-MacBook-Air-3.local>
2014-11-15 03:32:07 -0600

    Finished LyricsScraper
    LyricsScraper now takes in url, and returns a block of text with /r and
    /n new line breaks. These have yet to be removed.

--- Kyle/LyricsScraper.py
+++ Kyle/LyricsScraper.py
@@ -6,9 +6,21 @@ __author__ = 'kylejablon'
 from lxml import html
 import requests
 
+
+
+def lyricsscraper(url):
+    page = requests.get(url)
+    tree = html.fromstring(page.text)
+
+    unmodifiedlyrics = tree.xpath('//*[@id="main"]/div[3]/text()')
+    print unmodifiedlyrics
+
+    blocklyrics = ''
+    blocklyrics = ''.join(unmodifiedlyrics)
+    print blocklyrics
+    return blocklyrics
+
+
 #This is our test lyrics page, we will use azlyrics.com
 lyricspage = 'http://www.azlyrics.com/lyrics/taylorswift/blankspace.html'
-page = request.get(lyricspage)
-tree = html.fromstring(page.text)
-
-unmodifiedlyrics = tree.xpath('//')
+print lyricsscraper(lyricspage)
