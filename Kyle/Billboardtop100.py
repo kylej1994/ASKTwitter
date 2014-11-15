e9b2f9245a777b16c951463057d7c8793ccb30b9
kylej1994 <kylejablon@Kyles-MacBook-Air-3.local>
2014-11-15 02:47:56 -0600

    Finished Billboard top 100 Scrape
    #TYBASEDGOD

---
+++ Kyle/Billboardtop100.py
@@ -0,0 +1,37 @@
+__author__ = 'kylejablon'
+#This code scrapes the billboard top 100 using xpath and returns an array of the names of the top 100 songs
+
+#xpath ipmorts
+from lxml import html
+import requests
+
+##test code courtesy of http://docs.python-guide.org/en/latest/scenarios/scrape/
+# page = requests.get('http://econpy.pythonanywhere.com/ex/001.html')
+# tree = html.fromstring(page.text)
+# #This will create a list of buyers:
+# buyers = tree.xpath('//div[@title="buyer-name"]/text()')
+# #This will create a list of prices
+# prices = tree.xpath('//span/@class')
+# print buyers
+# print prices
+
+
+page = requests.get('http://www.billboard.com/charts/hot-100/2013-10-05')
+tree = html.fromstring(page.text)
+
+
+#Collect array of song number in format of "row-#{songnumber}"
+songsnumber = tree.xpath('//article/@id')
+#Collect array of song names in format of "Song Hover-#{SongName}"
+songhovernames = tree.xpath('//article/@data-hovertracklabel')
+#Because our collection of song names is in an incoherent format, we must fix it to just be an array of song names
+
+
+#Now create the final array of the songs. First create an empty array, then add the concatenate dversion of the new songs
+songs = []
+i = 0
+for song in songhovernames:
+    songs.append(song[11:])
+    i += 1
+
+
