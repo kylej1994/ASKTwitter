de7c503608c399f4353e1ab04cb33e43a0705125
kylej1994 <kylejablon@Kyles-MacBook-Air-3.local>
2014-11-15 18:09:42 -0600

    updated lyriscscraper.py

--- Kyle/LyricsScraper.py
+++ Kyle/LyricsScraper.py
@@ -43,15 +43,11 @@ def lyricSearcher(songs):
 
 #for each song, add its values to the dictionary
     for song in songs:
-        inputElement = driver.find_element_by_name("q") #find the search element
-        inputElement.send_keys(song) #send a qeury for the song name
         try:
+            inputElement = driver.find_element_by_name("q") #find the search element
+            inputElement.send_keys(song) #send a qeury for the song name
             inputElement.submit() #submit queury
-        except(Exception):
-            driver.get(baseurl)
-            continue
         #click on song.
-        try:
             songLink = driver.find_element_by_link_text(song)
             songLink.click()
         except(Exception):
@@ -72,14 +68,14 @@ def lyricSearcher(songs):
 
 #Four arbitrarily chosen weeks for billboard database building
 top2011 = "http://www.billboard.com/charts/hot-100/2011-02-19"
-top2012 = "http://www.billboard.com/charts/hot-100/2012-04-28"
-top2013 = "http://www.billboard.com/charts/hot-100/2013-10-05"
-top2014 = "http://www.billboard.com/charts/hot-100/2014-02-22"
+# top2012 = "http://www.billboard.com/charts/hot-100/2012-04-28"
+# top2013 = "http://www.billboard.com/charts/hot-100/2013-10-05"
+# top2014 = "http://www.billboard.com/charts/hot-100/2014-02-22"
 dict2011 = lyricSearcher(bb.billboardscrape(top2011))
-dict2012 = lyricSearcher(bb.billboardscrape(top2012))
-dict2013 = lyricSearcher(bb.billboardscrape(top2013))
-dict2014 = lyricSearcher(bb.billboardscrape(top2014))
-songdatabase = dict(dict2011.items() + dict2012.items() + dict2013.items() + dict2014.items())
+# dict2012 = lyricSearcher(bb.billboardscrape(top2012))
+# dict2013 = lyricSearcher(bb.billboardscrape(top2013))
+# dict2014 = lyricSearcher(bb.billboardscrape(top2014))
+songdatabase = dict2011 #dict(dict2011.items() + dict2012.items() + dict2013.items() + dict2014.items())
 # songdatabase = lyricSearcher(["Blank Space", "Turn Down For What"])
 sys.stdout = open('file.txt', 'w')
 for key in songdatabase.keys():
